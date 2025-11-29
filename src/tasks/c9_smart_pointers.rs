253cc4a8cec5af299a6cd944864a2c6d63fba682
Alexander Koshelev <alexander.y.koshelev@gmail.com>
2025-11-29 16:48:58 +0300

    c9: ex2

--- src/tasks/c9_smart_pointers.rs
+++ src/tasks/c9_smart_pointers.rs
@@ -17,6 +17,8 @@
 //
 // Use `Box` if needed
 
+use std::rc::Rc;
+
 // IMPLEMENT HERE:
 pub struct BinaryTreeNode {
     value: i32,
@@ -72,10 +74,67 @@ impl BinaryTreeNode {
 // Use `Rc` in the `Package` struct where needed to avoid deep clone.
 
 // IMPLEMENT HERE:
+pub struct Package {
+    name: String,
+    dependencies: Vec<Rc<Package>>,
+}
+
+impl Package {
+    pub fn new(name: &str) -> Self {
+        Package {
+            name: name.to_string(),
+            dependencies: Vec::new(),
+        }
+    }
+
+    pub fn with_dependencies(name: &str, dependencies: Vec<Rc<Package>>) -> Self {
+        Package { name: name.to_string(), dependencies }
+    }
+
+    pub fn list_dependencies(&self) -> Vec<String> {
+        let mut result = Vec::new();
+        let mut visited = std::collections::HashSet::new();
+
+        self._collect_dependencies(&mut result, &mut visited);
+        result
+    }
+
+    fn _collect_dependencies(
+        &self,
+        result: &mut Vec<String>,
+        visited: &mut std::collections::HashSet<String>,
+    ) {
+        for dep in &self.dependencies {
+            if !visited.contains(&dep.name) {
+                visited.insert(dep.name.clone());
+                result.push(dep.name.clone());
+                dep._collect_dependencies(result, visited);
+            }
+        }
+    }
+}
 
 #[test]
 fn test_list_dependencies() {
-    // IMPLEMENT HERE:
+    let std_lib = Rc::new(Package::new("std"));
+    let net_lib = Rc::new(Package::new("net"));
+
+    let web_server =
+        Rc::new(Package::with_dependencies("web-server", vec![std_lib.clone(), net_lib.clone()]));
+
+    let database = Rc::new(Package::with_dependencies("database", vec![std_lib.clone()]));
+
+    let app = Package::with_dependencies("app", vec![web_server.clone(), database.clone()]);
+
+    let dependencies = app.list_dependencies();
+
+    assert!(dependencies.contains(&"web-server".to_string()));
+    assert!(dependencies.contains(&"database".to_string()));
+    assert!(dependencies.contains(&"std".to_string()));
+    assert!(dependencies.contains(&"net".to_string()));
+
+    let std_count = dependencies.iter().filter(|&s| s == "std").count();
+    assert_eq!(std_count, 1, "std library should appear only once");
 }
 
 // RefCell
